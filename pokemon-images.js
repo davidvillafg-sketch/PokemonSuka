e2194764027bf8e7a95aeafd32276e67cb667285
davidvillafg-sketch <davidvillafg@gmail.com>
2026-08-18 10:16:43 +0200

    Add files via upload

---
+++ pokemon-images.js
@@ -0,0 +1,38 @@
+/*
+ * Catalogo immagini Pokémon.
+ *
+ * Per aggiungere un'immagine:
+ * 1. carica il file dentro ./images/pokemon/
+ * 2. aggiungi qui il nome esatto del Pokémon e il percorso del file
+ *
+ * Esempio:
+ *   "Pie": "./images/pokemon/pie.png",
+ *
+ * Sono preferibili PNG o WebP. Il nome del file può essere diverso dal nome
+ * del Pokémon, ma il nome a sinistra deve corrispondere esattamente a quello
+ * presente in game.js.
+ */
+window.POKEMON_IMAGE_PATHS = {
+    "Pie": "./images/pokemon/pie.png",
+    "Isco": "./images/pokemon/isco.png",
+    "Feb": "./images/pokemon/feb.png",
+    "Giana": "./images/pokemon/giana.png",
+    "Poppe": "./images/pokemon/poppe.png",
+    "Andra": "./images/pokemon/andra.png",
+    "Steo": "./images/pokemon/steo.png",
+    "Lalla": "./images/pokemon/lalla.png",
+    "Ksenija": "./images/pokemon/ksenija.png",
+    "Franca": "./images/pokemon/franca.png",
+    "Fera": "./images/pokemon/fera.png",
+    "Altini": "./images/pokemon/altini.png",
+    "Matte Iliev": "./images/pokemon/matte-iliev.png",
+    "Ressa": "./images/pokemon/ressa.png",
+    "Becchio": "./images/pokemon/becchio.png",
+    "Salvo": "./images/pokemon/salvo.png",
+    "Rebi": "./images/pokemon/rebi.png",
+    "Mimmuzzo": "./images/pokemon/mimmuzzo.png",
+    "Sara": "./images/pokemon/sara.png",
+    "Elena": "./images/pokemon/elena.png",
+    "Brillo": "./images/pokemon/brillo.png",
+    "Murru": "./images/pokemon/murru.png"
+};
--- sw.js
+++ sw.js
@@ -11,13 +11,38 @@
  * il browser scarica una nuova copia di sw.js quando GitHub Pages la aggiorna.
  */
 
-const CACHE_NAME = 'pokemon-suka-shell-v4';
+const CACHE_NAME = 'pokemon-suka-shell-v5';
 
 const APP_SHELL = [
   './',
   './index.html',
-  './game.js?v=single-player-1',
   './manifest.json',
+  './pokemon-images.js',
+  './game.js',
+  './styles.css',
+  './pwa.js',
+  './images/pokemon/pie.png',
+  './images/pokemon/isco.png',
+  './images/pokemon/feb.png',
+  './images/pokemon/giana.png',
+  './images/pokemon/poppe.png',
+  './images/pokemon/andra.png',
+  './images/pokemon/steo.png',
+  './images/pokemon/lalla.png',
+  './images/pokemon/ksenija.png',
+  './images/pokemon/franca.png',
+  './images/pokemon/fera.png',
+  './images/pokemon/altini.png',
+  './images/pokemon/matte-iliev.png',
+  './images/pokemon/ressa.png',
+  './images/pokemon/becchio.png',
+  './images/pokemon/salvo.png',
+  './images/pokemon/rebi.png',
+  './images/pokemon/mimmuzzo.png',
+  './images/pokemon/sara.png',
+  './images/pokemon/elena.png',
+  './images/pokemon/brillo.png',
+  './images/pokemon/murru.png',
   './icon-180.png',
   './icon-192.png',
   './icon-512.png'
@@ -28,7 +53,15 @@ const APP_SHELL = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
+      .then(cache =>
+        // Le icone sono opzionali: se non sono presenti nella repository,
+        // il gioco deve comunque poter installare la cache offline.
+        Promise.all(
+          APP_SHELL.map(asset =>
+            cache.add(asset).catch(() => undefined)
+          )
+        )
+      )
   );
 });
 
